993cb395d4ca66f6360d94292c4821d4f724b8e2
nishino <nishino@asojuku.ac.jp>
2025-02-27 11:11:31 +0900

    initial

---
+++ src/main/java/jp/ac/asojuku/asomon/AsomonApplication.java
@@ -0,0 +1,13 @@
+package jp.ac.asojuku.asomon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsomonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsomonApplication.class, args);
+	}
+
+}
---
+++ src/main/java/jp/ac/asojuku/asomon/ServletInitializer.java
@@ -0,0 +1,13 @@
+package jp.ac.asojuku.asomon;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AsomonApplication.class);
+	}
+
+}
